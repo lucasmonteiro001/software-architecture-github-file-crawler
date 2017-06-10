43cccc10aeb4af39fa5f9e83d236ea7b8c28c455
Lucas Monteiro <lucasmonteiro001@gmail.com>
2017-06-10 20:57:44 -0300

    Refactor to hash file_path and save for more than one file per repo

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ from github import Github
 import base64
 import json
 import os
+import hashlib
 
 GH = Github()
 
 
 class RepositoryHelper(object):
-    instace = 0
+    instance = 0
 
     def __init__(self, repo_name, path):
         super(RepositoryHelper, self).__init__()
         self.repo_name = str(repo_name)
         self.path = str(path)
         self.repo = GH.get_repo(self.repo_name)
-        RepositoryHelper.instace += 1
+        RepositoryHelper.instance += 1
 
     def get_file_content(self):
         file_content = self.repo.get_file_contents(self.path, "master")
@@ -22,16 +23,22 @@ class RepositoryHelper(object):
 
     def save_to_file(self):
 
-        output_path = 'output/' + self.repo_name.replace("/", "__") + '.yml'
+        hashed_path = hashlib.md5(self.path).hexdigest()
+
+        output_path = 'output/' + self.repo_name.replace("/", "__") + "___" + hashed_path + '.yml'
 
         # Ignore if file exists
         if os.path.isfile(output_path):
-            print str(RepositoryHelper.instace) + " skipped: " + self.repo_name
+            print str(RepositoryHelper.instance) + " [skipped]: " + self.repo_name
         else:
-            f = open(output_path, 'w')
-            f.write(self.get_file_content())
-            f.close()
-            print str(RepositoryHelper.instace) + " " + self.repo_name + ": processed"
+            file_content = self.get_file_content()
+            if file_content == "":
+                print str(RepositoryHelper.instance) + " [empty]: " + self.repo_name
+            else:
+                f = open(output_path, 'w')
+                f.write(self.get_file_content())
+                f.close()
+                print str(RepositoryHelper.instance) + " [saved]: " + self.repo_name
 
     def __str__(self):
         return super(RepositoryHelper, self).__str__()
@@ -40,6 +47,9 @@ class RepositoryHelper(object):
 repositories = open('resultado-compilado-sem-virgula.json', 'r').read().split('\n')
 
 for repo in repositories:
-    repoJson = data = json.loads(repo)
-    repository = RepositoryHelper(repoJson['repo_name'], repoJson['path'])
-    repository.save_to_file()
+    try:
+        repoJson = data = json.loads(repo)
+        repository = RepositoryHelper(repoJson['repo_name'], repoJson['path'])
+        repository.save_to_file()
+    except Exception:
+        pass
